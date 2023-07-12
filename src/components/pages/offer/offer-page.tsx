3fb7681e9f587cf5f5aa5ef5d419e4167b54962f
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-12 21:40:51 +0500

    Создал компоненту header и связал страницы ссылками

--- src/components/pages/offer/offer-page.tsx
+++ src/components/pages/offer/offer-page.tsx
@@ -1,40 +1,10 @@
-function OfferPage(): JSX.Element {
+import React from 'react';
+import Header from '../../header/header.tsx';
+
+function OfferPage(): React.JSX.Element {
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width={81}
-                  height={41}
-                />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                  Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header/>
       <main className="page__main page__main--offer">
         <section className="offer">
           <div className="offer__gallery-container container">
@@ -278,7 +248,7 @@ function OfferPage(): JSX.Element {
                       your stay with at least{' '}
                       <b className="reviews__text-amount">50 characters</b>.
                     </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+                    <button className="reviews__submit form__submit button" type="submit" disabled={false}>Submit</button>
                   </div>
                 </form>
               </section>
@@ -310,7 +280,8 @@ function OfferPage(): JSX.Element {
                       <b className="place-card__price-value">€80</b>
                       <span className="place-card__price-text">/&nbsp;night</span>
                     </div>
-                    <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+                    <button className="place-card__bookmark-button place-card__bookmark-button--active button"
+                            type="button">
                       <svg className="place-card__bookmark-icon" width={18} height={19}>
                         <use xlinkHref="#icon-bookmark"/>
                       </svg>
