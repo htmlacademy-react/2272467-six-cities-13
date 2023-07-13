2f9d7a4094fb2dccf158c157f0385130dd878716
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-13 21:34:06 +0500

    Поменял ссылку на главную страницу в компоненте notFoundPage

--- src/components/not-found/not-found-page.tsx
+++ src/components/not-found/not-found-page.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { AppRoute } from '../../constants/app-route.ts';
 
 function NotFoundPage(): React.JSX.Element {
   return (
     <Fragment>
       <h1>404. Page not found</h1>
-      <Link to={'/'}> Go to main page</Link>
+      <Link to={AppRoute.Main}> Go to main page</Link>
     </Fragment>
   );
 }
