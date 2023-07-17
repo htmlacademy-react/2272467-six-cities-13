e690c4ec333881a9492826db956f983947a36c09
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-17 18:48:42 +0500

    Добавил компоненту ReviewForm и сохранение введенных данных в state

--- src/components/pages/offer/offer-page.tsx
+++ src/components/pages/offer/offer-page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import ReviewForm from '../../review-form/review-form.tsx';
 
 function OfferPage(): React.JSX.Element {
   return (
@@ -170,89 +171,7 @@ function OfferPage(): React.JSX.Element {
                     </div>
                   </li>
                 </ul>
-                <form className="reviews__form form" action="#" method="post">
-                  <label className="reviews__label form__label" htmlFor="review">
-                    Your review
-                  </label>
-                  <div className="reviews__rating-form form__rating">
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      defaultValue={5}
-                      id="5-stars"
-                      type="radio"
-                    />
-                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star"/>
-                      </svg>
-                    </label>
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      defaultValue={4}
-                      id="4-stars"
-                      type="radio"
-                    />
-                    <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star"/>
-                      </svg>
-                    </label>
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      defaultValue={3}
-                      id="3-stars"
-                      type="radio"
-                    />
-                    <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star"/>
-                      </svg>
-                    </label>
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      defaultValue={2}
-                      id="2-stars"
-                      type="radio"
-                    />
-                    <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star"/>
-                      </svg>
-                    </label>
-                    <input
-                      className="form__rating-input visually-hidden"
-                      name="rating"
-                      defaultValue={1}
-                      id="1-star"
-                      type="radio"
-                    />
-                    <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star"/>
-                      </svg>
-                    </label>
-                  </div>
-                  <textarea
-                    className="reviews__textarea form__textarea"
-                    id="review"
-                    name="review"
-                    placeholder="Tell how was your stay, what you like and what can be improved"
-                    defaultValue={''}
-                  />
-                  <div className="reviews__button-wrapper">
-                    <p className="reviews__help">
-                      To submit review please make sure to set{' '}
-                      <span className="reviews__star">rating</span> and describe
-                      your stay with at least{' '}
-                      <b className="reviews__text-amount">50 characters</b>.
-                    </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled={false}>Submit</button>
-                  </div>
-                </form>
+                <ReviewForm/>
               </section>
             </div>
           </div>
@@ -282,7 +201,8 @@ function OfferPage(): React.JSX.Element {
                       <b className="place-card__price-value">€80</b>
                       <span className="place-card__price-text">/&nbsp;night</span>
                     </div>
-                    <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+                    <button className="place-card__bookmark-button place-card__bookmark-button--active button"
+                            type="button">
                       <svg className="place-card__bookmark-icon" width={18} height={19}>
                         <use xlinkHref="#icon-bookmark"/>
                       </svg>
