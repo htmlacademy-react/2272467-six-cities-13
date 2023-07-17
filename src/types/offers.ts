07c2ed137557e9a61098a2f1ccd8d5c3799e91e1
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-17 16:40:06 +0500

    Добавил тип для типа аренды

--- src/types/offers.ts
+++ src/types/offers.ts
@@ -9,10 +9,12 @@ type TCity = {
   location: TLocation;
 }
 
+type TOfferType = 'Apartment' | 'Private Room' | 'House' | 'Hotel';
+
 export type TOffer = {
   id: string;
   title: string;
-  type: string;
+  type: TOfferType;
   price: number;
   city: TCity;
   location: TLocation;
