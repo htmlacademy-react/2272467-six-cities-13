c40a9d0f6bff63d72e549b6185109fd7371ea325
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-17 18:10:33 +0500

    Создал компоненту footer

--- src/components/pages/favorites/favorites-page.tsx
+++ src/components/pages/favorites/favorites-page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { TOffers } from '../../../types/offers.ts';
 import FavoritesList from '../../favorites-list/favorites-list.tsx';
+import Footer from '../../footer/footer.tsx';
 
 
 type TFavoritesPageProps = {
@@ -22,17 +23,7 @@ function FavoritesPage({ offers }: TFavoritesPageProps): React.JSX.Element {
           </section>
         </div>
       </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width={64}
-            height={33}
-          />
-        </a>
-      </footer>
+      <Footer/>
     </div>
   );
 }
