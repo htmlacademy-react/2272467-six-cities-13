5b88a7b2be3819e549fba6292874369df433d82f
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-17 16:18:01 +0500

    Добавил состояние активной карточки предложения

--- src/components/offer-card/offer-card.tsx
+++ src/components/offer-card/offer-card.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../constants/app-route.ts';
+import { TOffer } from '../../types/offers.ts';
 
-function OfferCard(): React.JSX.Element {
+type TOfferCardProps = {
+  offer: TOffer;
+}
+
+function OfferCard({ offer }: TOfferCardProps): React.JSX.Element {
+  const {
+    id, title, type, price,
+    isPremium, previewImage
+  } = offer;
+
+  const [activated, setActivated] = useState(id);
+
+  function addActive(e) {
+    setActivated(e) //TODO Переделать для отображения маркеров на карте
+  }
+
   return (
-    <article className="cities__card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
+    <article
+      className="cities__card place-card"
+      onMouseOver={addActive}
+    >
+      {isPremium &&
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"/>
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -33,9 +53,9 @@ function OfferCard(): React.JSX.Element {
           </div>
         </div>
         <h2 className="place-card__name">
-          <Link to={AppRoute.Offer}>Beautiful &amp; luxurious apartment at great location</Link>
+          <Link to={AppRoute.Offer}>{title}</Link>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
