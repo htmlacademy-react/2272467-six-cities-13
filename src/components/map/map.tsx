b1ebfdb5430691c097680c75fdd24f562388f953
Dmitriy Myakotin <demon4301@gmail.com>
2023-07-21 09:02:50 +0500

    Подключил свои маркеры

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -2,7 +2,9 @@ import React, { useRef } from 'react';
 import { TOffers } from '../../types/offers.ts';
 import useMap from '../../hooks/use-map.tsx';
 import 'leaflet/dist/leaflet.css';
+import leaflet from 'leaflet';
 import { city } from '../../mocks/city.ts';
+import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT } from '../../constants/marker.ts';
 
 type TMapProps = {
   offers: TOffers;
@@ -11,6 +13,18 @@ type TMapProps = {
 function Map({ offers }: TMapProps): React.JSX.Element {
   const mapRef = useRef(null);
   const map = useMap(mapRef, city);
+
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const currentCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_CURRENT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
 
   return (
     <div
