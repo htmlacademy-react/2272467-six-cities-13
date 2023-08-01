50070e7a3177ac7ae552d2b0446f5074a4b2bacc
Dmitriy Myakotin <demon4301@gmail.com>
2023-08-01 22:48:04 +0500

    Подключил редакс к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.tsx';
+import {Provider} from 'react-redux';
 import { offers } from './mocks/offers.ts';
 import { reviews } from './mocks/reviews.ts';
 import { cities } from './constants/city.ts';
+import {store} from './store';
 
 
 const root = ReactDOM.createRoot(
@@ -11,11 +13,13 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <App
-      offers={offers}
-      cities={cities}
-      reviews={reviews}
-    />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App
+        offers={offers}
+        cities={cities}
+        reviews={reviews}
+      />
+    </React.StrictMode>
+  </Provider>
 );
